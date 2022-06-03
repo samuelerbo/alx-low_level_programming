e3c977d1d74b67a61457f682a820fac4411e1722
SamErbo <samuel.ermias@gmail.com>
2022-06-02 19:48:27 -0700

    sove task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+nclude <stdio.h>
+/**
+ *  * main - Prints "Programming is like building a multilingual
+ *   * puzzle, followed by a new line."
+ *    *
+ *     * Return: Always 0.
+ *      */
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
