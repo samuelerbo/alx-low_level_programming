777d0966550940f85af2325c543bb5d2e4c890d5
SamErbo <samuel.ermias@gmail.com>
2022-06-03 19:22:45 -0700

    finish task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * Description: main -print alphabet in lowercase
+ * Return: retun 0 if everything is ok
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	for (c; c <= 'z'; c++)
+	{
+		putchar(c)'
+	}
+
+	return (0);
+}
+
