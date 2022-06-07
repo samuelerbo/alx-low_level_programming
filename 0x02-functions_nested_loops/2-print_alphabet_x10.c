120fa3eed715800517934cd0679c7d5fdac82d5a
SamErbo <samuel.ermias@gmail.com>
2022-06-07 16:46:19 -0700

    fix task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+	int a = 0;
+
+	while(a<10)
+	{
+		 char c;
+
+        	for (c = 'a'; c <= 'z'; c++)
+        	{
+                _putchar(c);
+        	}
+        _putchar('\n');
+	a++;
+	}
+	_putchar('\n');
+}
