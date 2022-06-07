acd191edc558e3c7b27ad538eea853ba8156ff0c
SamErbo <samuel.ermias@gmail.com>
2022-06-07 15:25:30 -0700

    fix task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,18 @@
  * Return: return 0 (successful)
  */
 
-void main(void){
-	_putchar ('_');
-	_putchar ('p');
-	_putchar ('u');
-	_putchar ('t');
-	_putchar ('c');
-	_putchar ('h');
-	_putchar ('a');
-	_putchar ('r');
+void main(void)
+{
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	-putchar('\n');
 
 	return (0);
 }
