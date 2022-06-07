4c71bc02beda6dbef57cd455c6a306c500c0df53
SamErbo <samuel.ermias@gmail.com>
2022-06-07 09:32:10 -0700

    solve task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.c"
+
+/**
+ * main - prints _putchar
+ *
+ * Return: return 0 (Success)
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
