4733d1de1a11556b79431def4abbc090585d2f96
SamErbo <samuel.ermias@gmail.com>
2022-06-07 17:09:35 -0700

    fix betty style and documentation error

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,24 @@
 #include "main.h"
 
+/*
+ * print_alphabet_x10 : 10 times print alphabet
+ *
+ * Return - return 0 (successful)
+ */
+
 void print_alphabet_x10(void)
 {
 	int a = 0;
 
-	while(a<10)
+	while (a < 10)
 	{
-		 char c;
+		char c;
 
-        	for (c = 'a'; c <= 'z'; c++)
-        	{
-                _putchar(c);
-        	}
-        _putchar('\n');
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+	_putchar('\n');
 	a++;
 	}
 }
