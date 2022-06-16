ab4eb6b6649e72aedcb53ad96c89182157d9a337
Samuel Erbo <33570394+SamErbo@users.noreply.github.com>
2022-06-16 11:50:03 +0300

    solve task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+/**
+ * _strcmp - compare 2 string
+ * @s1:string argument for _strcmp
+ * @s2:strmp argument for _strcmp
+ * Return:int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (*s1 - *s2);
+	}
+}
