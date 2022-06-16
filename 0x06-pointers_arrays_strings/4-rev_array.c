905f9877c46f6f4e87c41320c8d830e1b80d3959
Samuel Erbo <33570394+SamErbo@users.noreply.github.com>
2022-06-16 11:51:45 +0300

    solve task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+/**
+ * reverse_array - reverse array
+ * @a:array argument for reverse_array
+ * @n:integer argument for reverse_array
+ * Return:void
+ */
+void reverse_array(int *a, int n)
+{
+int i, c;
+
+for (i = 0; (i < (n - 1) / 2); i++)
+	{
+	c = a[i];
+	a[i] = a[n - 1 - i];
+	a[n - 1 - i] = c;
+	}
+}
