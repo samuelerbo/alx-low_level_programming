d0ccdb21dbb2eea30de5945891a1b9cab10d8bcc
SamErbo <samuel.ermias@gmail.com>
2022-06-22 21:35:51 -0700

    solve task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * _puts_recursion - prints a string followed by a new line
+  * @s: string parameter
+  * Return: Return 0 if successful
+  */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
