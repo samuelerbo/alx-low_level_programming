d06625ed3f40615b2e9041f21585abeba8238f34
Samuel Erbo <samuel.ermias@gmail.com>
2022-09-03 03:32:40 +0300

    fix bug

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -22,6 +22,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->n = n;
 	new_node->next = *head;
 	new_node->prev = NULL;
+	if(*head != NULL)
+		(*head)->prev = new;
 	*head = new_node;
 
 	return (new_node);
