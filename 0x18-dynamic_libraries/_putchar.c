bc6a39d3c8ff4214fd12d3a9e2f8773fb7934516
Samuel Erbo <samuel.ermias@gmail.com>
2022-09-20 23:19:55 +0300

    solve all tasks

---
+++ 0x18-dynamic_libraries/_putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int _putchar(char c)
+{
+	putchar('H');
+	putchar('0');
+	putchar('L');
+	putchar('L');
+	putchar('0');
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x18-dynamic_libraries/_puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void _puts(char *s)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('s');
+}
